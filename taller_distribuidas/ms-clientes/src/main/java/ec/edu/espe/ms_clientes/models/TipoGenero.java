4c840e43cfb5213c82619a389f7830b2cc561446
Carlos Sebastián Rivera Espín <95983843+csrivera3@users.noreply.github.com>
2026-02-16 08:56:14 -0500

    listado

--- taller_distribuidas/ms-clientes/src/main/java/ec/edu/espe/ms_clientes/models/TipoGenero.java
+++
@@ -1,5 +0,0 @@
-package ec.edu.espe.ms_clientes.models;
-
-public enum TipoGenero {
-    MASCULINO, FEMENINO, VILLA
-}
--- taller_distribuidas/ms-clientes/src/main/java/ec/edu/espe/ms_clientes/models/TipoTraccion.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.espe.ms_clientes.models;
-
-public enum TipoTraccion {
-    DELANTERA,
-    TRASERA,
-    INTEGRAL_4WD,
-    AWD // All Wheel Drive
-}
